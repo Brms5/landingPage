2ef2c9f3047d21ab35a005a5e19bb3f96d607c6c
Bruno <bruno.moreira@meta.com.br>
2023-05-27 00:09:41 -0300

    feat: starting the initial structure development.

---
+++ index.js
@@ -0,0 +1,20 @@
+// Get references to the buttons
+var homeBtn = document.getElementById("home-btn");
+var postsBtn = document.getElementById("posts-btn");
+var aboutBtn = document.getElementById("about-btn");
+
+// Add click event listeners
+homeBtn.addEventListener("click", function () {
+  // Perform action for the Home button
+  console.log("Home button clicked");
+});
+
+postsBtn.addEventListener("click", function () {
+  // Perform action for the Posts button
+  console.log("Posts button clicked");
+});
+
+aboutBtn.addEventListener("click", function () {
+  // Perform action for the About button
+  console.log("About button clicked");
+});
